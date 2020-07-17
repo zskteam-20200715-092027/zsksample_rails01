ff556b4f08ae4918b4a286ebb06fc7ac1c9d4d03
Sumi Masaaki <52375299+summas@users.noreply.github.com>
2020-07-17 20:14:56 +0900

    test: feat: Add tests of user model (#19)
    * test: feat: Add user model test
    
    * test: feat: Add a test for the nil case.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name, and email' do
+    user = User.new(
+      name: 'testName',
+      email: 'test@example.com'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'is valid without a name, and email' do
+    user = User.new(
+      name: nil,
+      email: nil
+    )
+    expect(user).to be_valid
+  end
 end
